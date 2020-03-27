8de867fd459cf016853c82c32292eccdfc5266f5
Matteo Pugliese <stenwie91@hotmail.it>
2020-03-27 17:02:04 +0100

    ADD markdown-pages pt.2 -> now in the homepage will map all files insert inside markdown-page folder in src

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,47 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
-import Image from '../components/image';
 import SEO from '../components/seo';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-);
+export default ({ data }) => {
+  const {
+    allMarkdownRemark: { totalCount, edges },
+  } = data;
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div>
+        <h1>I Miei Pensieri</h1>
+        <h4>{totalCount}</h4>
+        {edges.map(({ node: { id, frontmatter: { title, date }, excerpt } }) => (
+          <div key={id}>
+            <span>
+              {title} - {date}
+            </span>
+            <p>{excerpt}</p>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  );
+};
 
-export default IndexPage;
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            date
+            description
+            title
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
